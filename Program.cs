ebd8aed9f7ef73fc99652a43730a5d6cfe6f29da
triva <tristan.hasqeunoph@gmail.com>
2020-10-20 15:30:32 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // saisie d'une phrase 
+            Console.WriteLine("Entrez une phrase : ");
+            string phrase = Console.ReadLine();
+
+            // replacement des espace apr underscore
+            string newpharse = phrase.Replace(' ','_');
+
+            //affichage de la nouvelle phrase
+            Console.WriteLine(newpharse);
+            Console.ReadLine();
+        }
+    }
+}
